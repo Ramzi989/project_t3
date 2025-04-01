860383ae7cbd27433c1c722afd3a1dfe4ae8c70e
Karim <karim.aglain@edu-kw1c.nl>
2025-04-01 10:15:53 +0200

    mappen

--- Pages/enquete.php
+++ Pages/enquete.php
@@ -1,1 +1,18 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../styles/HomePagina.css">
+        <title>Enquete</title>
+    </head>
+    <body>
+        <div class="enquete-container">
+                <div class="enquete-inhoud">
+
+                </div>
+        </div>
+    </body>
+</html>
